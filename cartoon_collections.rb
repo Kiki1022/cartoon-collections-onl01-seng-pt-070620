56b8520fada6f28f8a859ee41c75d61ba05e61c3
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-17 19:07:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,12 @@ def summon_captain_planet(planeteer_calls)
 
 end
 
-def long_planeteer_calls()
+def long_planeteer_calls(short_words)
+  if ()
+    false 
+  else
+    true 
+ end   
 end
 
 def find_the_cheese# code an argument here
