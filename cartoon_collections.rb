20bb4b86bb9fe570c53fedce5343c12ebc44616f
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-17 19:50:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,7 @@ def long_planeteer_calls(short_words)
 end
 
 def find_the_cheese(array)
+  
   array.find {|element| cheese_types.include?(element)}
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
