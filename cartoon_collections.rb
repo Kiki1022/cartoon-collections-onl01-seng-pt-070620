74666922814f3fbd513226a7772a8aef55af8c1a
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-17 19:39:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(short_words)
   short_words.any? {|string| string.length> 4} 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
